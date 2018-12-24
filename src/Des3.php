84c499f18a98de4571f309cb8ec23271902e9460
YouthAge <38289888+YouthAge@users.noreply.github.com>
2018-12-24 16:54:09 +0800

    Update Des3.php
    增加动态试验key和iv

--- src/Des3.php
+++ src/Des3.php
@@ -23,21 +23,25 @@ class Des3 {
      */
     public function __construct(string $key=null, string $iv=null)
     {
-        if (strlen($key) != 24){
+        if (!$key && strlen($key) != 24){
             throw new \Exception("DES3_KEY长度错误，长度为24");
         }
-        if (strlen($iv) != 8){
+        if (!$iv && strlen($iv) != 8){
             throw new \Exception("DES3_IV长度错误，长度为8");
         }
         $this->key = $key;
         $this->iv = $iv;
     }
 
-    public function encrypt($input){
+    public function encrypt($input, $key='', $iv=''){
+        $this->key = $key ? $key : $this->key;
+		$this->iv = $iv ? $iv : $this->iv;
         return base64_encode(openssl_encrypt($input, "des-ede3-cbc", $this->key, OPENSSL_RAW_DATA, $this->iv));
     }
 
-    public function decrypt($encrypted){
+    public function decrypt($encrypted, $key='', $iv=''){
+        $this->key = $key ? $key : $this->key;
+		$this->iv = $iv ? $iv : $this->iv;
         return openssl_decrypt(base64_decode($encrypted), 'des-ede3-cbc', $this->key, OPENSSL_RAW_DATA, $this->iv);
     }
 
